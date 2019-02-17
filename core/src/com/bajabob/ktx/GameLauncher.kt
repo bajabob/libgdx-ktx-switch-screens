3e9c6632b61ac1831435511bc79a1adcf43cec06
Bob Timm <bajabob.tx@gmail.com>
2019-02-17 13:06:41 -0600

    remove all assets, good working template

---
+++ core/src/com/bajabob/ktx/GameLauncher.kt
@@ -0,0 +1,17 @@
+package com.bajabob.ktx
+
+import com.badlogic.gdx.Game
+
+open class GameLauncher : Game() {
+
+    override fun create() {
+
+    }
+
+    override fun render() {
+
+    }
+
+    override fun dispose() {
+    }
+}
